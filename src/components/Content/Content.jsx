e0c4def368c9b6e9c519f5b74a6cc69f5a4b0eff
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-03-11 17:35:46 +0500

    finish share part

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,3 +1,50 @@
+import image from "../../assets/images/share.png";
+
 export const Content = () => {
-  return <section>Content</section>;
+  return (
+    <section className="content py-10">
+      <div className="container">
+        <div className="share__text-wrapper min-[480px]:max-w-[75%] min-[1024px]:max-w-[929px] mx-auto flex flex-col items-center min-[1024px]:mb-[190px] min-[768px]:mb-36 mb-16">
+          <strong className="block text-center font-semibold text-[#3D82F2] min-[768px]:text-[25px] min-[768px]:leading-[38px]">STEP 1</strong>
+          <h2 className="text-white text-center  min-[1280px]:text-[75px] min-[1280px]:leading-[112px] text-xl font-bold min-[375px]:text-2xl min-[768px]:text-4xl min-[1024px]:text-5xl">
+            Connect Your Content
+          </h2>
+          <p className="text-[#707070] text-center text-[8px] min-[375px]:text-[10px] min-[640px]:text-base min-[791px]:text-lg min-[1024px]:text-[30px] min-[1024px]:leading-[45px] mb-3 min-[768px]:mb-[34px]">
+            Bring all of your content together and get a Homepage that automatically updates whenever you create anywhere online.
+          </p>
+          <a
+            className="text-[#707070] border border-[#707070] inline-block text-xs min-[768px]:text-xl font-semibold p-3 min-[768px]:p-6 min-[768px]:min-w[313px] rounded-[10px] hover:border-white hover:text-white active:opacity-80 transition"
+            href="#"
+          >
+            View Available Sources
+          </a>
+        </div>
+        <div className="share__content justify-center grid max-[767px]:grid-rows-2 min-[768px]:grid-cols-2 gap-6">
+          <div className="text-white flex flex-col items-center max-w-[686px] min-[1419px]:items-start shrink-0">
+            <strong className="font-medium text-sm min-[1024px]:mb-20 mb-7 min-[768px]:text-[30px] min-[768px]:leading-[45px]text-left">Your Homepage</strong>
+            <div className="title-wrapper min-[480px]:max-w-[75%] min-[1024px]:max-w-full">
+              <h3 className="text-white text-center min-[1280px]:text-[75px] min-[1280px]:leading-[112px] text-xl font-bold min-[375px]:text-2xl min-[768px]:text-4xl min-[768px]:text-left min-[1024px]:text-5xl mb-5 min-[768px]:mb-[27px]">
+                Your Content.
+                <br /> All in <mark className="bg-transparent text-[#3D82F2]">One Spot</mark>
+              </h3>
+              <ol className="text-sm max-[767px]:text-justify min-[768px]:text-xl min-[1024px]:text-[23px] min-[768px]:leading-[34px]">
+                <li className="before:bg-[#2D2D2D] before:w-[33px] before:rounded-full before:content-['1'] before:inline-flex before:justify-center before:me-3 mb-4">
+                  Bring all of your content together into one homepage.
+                </li>
+                <li className="before:bg-[#2D2D2D] before:w-[33px] before:rounded-full before:content-['2'] before:inline-flex before:justify-center before:me-3">
+                  Your page automatically updates whenever you create.
+                </li>
+              </ol>
+            </div>
+          </div>
+          <img
+            className="-order-1 min-[768px]:mb-0 min-[768px]:order-1 max-[767px]:max-w-lg min-[768px]:translate-x-6 justify-self-start max-[767px]:w-full max-[767px]:mx-auto"
+            src={image}
+            width={820}
+            alt="In the image showed Charlie Puth and his music"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -1,49 +1,36 @@
-import image from "../../assets/images/share.png";
+import image from "../../assets/images/onelink.png";
 
 export const Share = () => {
   return (
-    <section className="share">
+    <section className="share py-10">
       <div className="container">
         <div className="share__text-wrapper min-[480px]:max-w-[75%] min-[1024px]:max-w-[929px] mx-auto flex flex-col items-center min-[1024px]:mb-[190px] min-[768px]:mb-36 mb-16">
-          <strong className="block text-center font-semibold text-[#3D82F2] min-[768px]:text-[25px] min-[768px]:leading-[38px]">STEP 1</strong>
+          <strong className="block text-center font-semibold text-[#3D82F2] min-[768px]:text-[25px] min-[768px]:leading-[38px]">STEP 2</strong>
           <h2 className="text-white text-center  min-[1280px]:text-[75px] min-[1280px]:leading-[112px] text-xl font-bold min-[375px]:text-2xl min-[768px]:text-4xl min-[1024px]:text-5xl">
-            Connect Your Content
+            Share Your Homepage
           </h2>
           <p className="text-[#707070] text-center text-[8px] min-[375px]:text-[10px] min-[640px]:text-base min-[791px]:text-lg min-[1024px]:text-[30px] min-[1024px]:leading-[45px] mb-3 min-[768px]:mb-[34px]">
-            Bring all of your content together and get a Homepage that automatically updates whenever you create anywhere online.
+            Share your Wavium homepage link with your followers, and we'll handle the rest.
           </p>
-          <a
-            className="text-[#707070] border border-[#707070] inline-block text-xs min-[768px]:text-xl font-semibold p-3 min-[768px]:p-6 min-[768px]:min-w[313px] rounded-[10px] hover:border-white hover:text-white active:opacity-80 transition"
-            href="#"
-          >
-            View Available Sources
-          </a>
         </div>
-        <div className="share__content justify-center grid max-[768px]:grid-rows-2 min-[768px]:grid-cols-2 gap-6">
-          <div className="text-white flex flex-col items-center max-w-[686px] min-[1419px]:items-start shrink-0">
-            <strong className="font-medium text-sm min-[1024px]:mb-20 mb-7 min-[768px]:text-[30px] min-[768px]:leading-[45px]text-left">Your Homepage</strong>
-            <div className="title-wrapper min-[480px]:max-w-[75%] min-[1024px]:max-w-full">
-              <h3 className="text-white text-center min-[1280px]:text-[75px] min-[1280px]:leading-[112px] text-xl font-bold min-[375px]:text-2xl min-[768px]:text-4xl min-[768px]:text-left min-[1024px]:text-5xl">
-                Your Content.
-                <br /> All in <mark className="bg-transparent text-[#3D82F2]">One Spot</mark>
-              </h3>
-              <ol className="text-sm max-[767px]:text-justify min-[768px]:text-xl min-[1024px]:text-[23px] min-[768px]:leading-[34px]">
-                <li className="before:bg-[#2D2D2D] before:w-[33px] before:rounded-full before:content-['1'] before:inline-flex before:justify-center before:me-3 mb-4">
-                  Bring all of your content together into one homepage.
-                </li>
-                <li className="before:bg-[#2D2D2D] before:w-[33px] before:rounded-full before:content-['2'] before:inline-flex before:justify-center before:me-3">
-                  Your page automatically updates whenever you create.
-                </li>
-              </ol>
-            </div>
-          </div>
-          <img
-            className="-order-1 min-[768px]:mb-0 min-[768px]:order-1 max-[767px]:max-w-lg min-[768px]:translate-x-6 justify-self-start max-[767px]:w-full max-[767px]:mx-auto"
-            src={image}
-            width={820}
-            alt="In the image showed Charlie Puth and his music"
-          />
-        </div>
+        <ul className="share__content grid max-[767px]:grid-rows-2 min-[768px]:grid-cols-2 gap-[23px]">
+          <li className="flex flex-col pt-5 pl-7 min-[768px]:pt-[33px] min-[768px]:pl-[45px] bg-[#0D0D0D]">
+            <strong className="text-[#0FC65C] font-normal mb-3 min-[768px]:mb-6 min-[768px]:text-xl min-[1024px]:text-[30px] min-[1024px]:leading-[45px]">One link</strong>
+            <h3 className="text-white font-semibold text-4xl min-[768px]:text-5xl min-[1280px]:text-[80px] grow min-[1280px]:leading-[120px] mb-[14px]">
+              <mark className="bg-transparent block text-[#919191]">ALL You Create.</mark> One Link
+            </h3>
+            <img className="w-5/6 self-end" src={image} alt="" />
+          </li>
+          <li className="flex flex-col pt-5 pl-7 min-[768px]:pt-[33px] min-[768px]:pl-[45px] bg-[#0D0D0D]">
+            <strong className="text-[#0FC65C] font-normal mb-10 min-[1280px]:mb-[96px] min-[768px]:text-xl min-[1024px]:text-[30px] min-[1024px]:leading-[45px]">Collect Subscribers</strong>
+            <h3 className="text-white font-semibold text-4xl min-[768px]:text-5xl min-[1280px]:text-[80px] grow min-[1280px]:leading-[120px] mb-[14px]">
+              <mark className="bg-transparent block text-[#919191]">
+                Emails. <br /> Phone #s.
+              </mark>{" "}
+              All Yours.
+            </h3>
+          </li>
+        </ul>
       </div>
     </section>
   );
