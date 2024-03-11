8d333227f0fdc8330b2bb6507b46e031b8fa49d5
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-03-11 18:42:28 +0500

    expert finished

--- src/components/Expert/Expert.jsx
+++ src/components/Expert/Expert.jsx
@@ -1,3 +1,12 @@
+import image from "../../assets/images/expert.png";
+
 export const Expert = () => {
-  return <section>Expert</section>;
+  return (
+    <section className="expert pt-10 pb-14 min-[1024px]:pt-20 min-[1024px]:pb-28">
+      <h2 className="text-white text-center  min-[1280px]:text-[75px] min-[1280px]:leading-[112px] text-xl font-bold min-[375px]:text-2xl min-[768px]:text-4xl min-[1024px]:text-5xl mb-5 min-[1024px]:mb-[52px]">
+        Experts Agree
+      </h2>
+      <img src={image} alt="" />
+    </section>
+  );
 };
