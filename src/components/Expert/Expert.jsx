51b11a46d10ed3c9c3586dee801c078ab581e84c
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-03-12 06:30:50 +0500

    helped finished and add max width for expert part

--- src/components/Expert/Expert.jsx
+++ src/components/Expert/Expert.jsx
@@ -2,7 +2,7 @@ import image from "../../assets/images/expert.png";
 
 export const Expert = () => {
   return (
-    <section className="expert pt-10 pb-14 min-[1024px]:pt-20 min-[1024px]:pb-28">
+    <section className="expert max-w-[1830px] mx-auto pt-10 pb-14 min-[1024px]:pt-20 min-[1024px]:pb-28">
       <h2 className="text-white text-center  min-[1280px]:text-[75px] min-[1280px]:leading-[112px] text-xl font-bold min-[375px]:text-2xl min-[768px]:text-4xl min-[1024px]:text-5xl mb-5 min-[1024px]:mb-[52px]">
         Experts Agree
       </h2>
